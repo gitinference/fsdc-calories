bedb9fc616137c08fcd5baa420755f75e817e44b
joel-alvarado <joel.alvarado2@upr.edu>
2024-06-25 16:07:48 -0400

    update chart folder name, added subdir for pie chart

--- update_data.py
+++ update_data.py
@@ -2,6 +2,7 @@ from scraper import download_latest_hts
 from hts_processor import hts_processor
 from charts import update_plate_charts
 
+
 def update_data():
     download_latest_hts()
     hts_processor()
