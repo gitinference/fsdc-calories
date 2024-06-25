64354a10f3aa82ea85a224bfa5ad9e8f874135fb
joel-alvarado <joel.alvarado2@upr.edu>
2024-06-25 15:40:56 -0400

    configured logger, scheduled update job, etc

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+import atexit
+
+from flask import Flask, request, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
+from routes.routes import routes
+from update_data import update_data
+
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 
+cron = BackgroundScheduler(daemon=True)
+cron.add_job(update_data, 'interval', seconds=5)
+cron.start()
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+atexit.register(lambda: cron.shutdown(wait=False))
 
 if __name__ == '__main__':
     app.run()
