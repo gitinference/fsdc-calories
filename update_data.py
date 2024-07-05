a7d319927d4dd9791452548405e02ac2d7356e1e
joel-alvarado <joel.alvarado2@upr.edu>
2024-07-05 10:32:52 -0400

    update_data now downloads import/export data

--- update_data.py
+++ update_data.py
@@ -1,10 +1,11 @@
-from scraper import download_latest_hts
+from scraper import download_latest_hts_imports, download_latest_hts_exports
 from generate_plate_data import generate_plate_data
 from charts import update_plate_charts
 
 
 def update_data():
-    download_latest_hts()
+    download_latest_hts_imports()
+    download_latest_hts_exports()
     generate_plate_data()
     update_plate_charts()
 
