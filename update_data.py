30cce50e145841bfcac0468cdcf1a8fa60c9d39c
joel-alvarado <joel.alvarado2@upr.edu>
2024-07-24 10:37:29 -0400

    fixed raw data being passed to net data processor

--- update_data.py
+++ update_data.py
@@ -1,12 +1,15 @@
 from scraper import get_hts_dataframe
 from generate_plate_data import generate_plate_data
-from generate_hts_macronutrients_data import generate_net_macronutrients_data
+from generate_hts_macronutrients_data import generate_net_macronutrients_data, generate_hts_macronutrients_data
 
 
 def update_data():
     raw_import, raw_export = get_hts_dataframe()
     generate_plate_data(raw_import)
-    generate_net_macronutrients_data(raw_import, raw_export)
+    processed_import = generate_hts_macronutrients_data(raw_import)
+    processed_export = generate_hts_macronutrients_data(raw_export)
+
+    net_data = generate_net_macronutrients_data(processed_import, processed_export)
 
 
 if __name__ == '__main__':
