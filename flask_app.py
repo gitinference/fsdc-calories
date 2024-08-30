8a3f7974d8ef2bfb591f1d55feb0a628247f4453
joel-alvarado <joel.alvarado2@upr.edu>
2024-08-30 19:47:59 -0400

    added localhost to CORS origins, changed dev port

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 app.register_blueprint(routes)
 CORS(app, resources={r"/*": {
     "origins": [
-        "http://127.0.0.1:5500",
+        "http://127.0.0.1:5000",  # Werkzeug WSGI Development Server
+        "http://localhost:5000",  # Werkzeug WSGI Development Server
         "http://localhost:63342",  # PyCharm HTML Live Server
         "https://www.uprm.edu"
     ]
