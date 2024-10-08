10560c824fe000b3ade025f94c3611b70c24e060
joel-alvarado <joel.alvarado2@upr.edu>
2024-10-08 05:51:39 -0400

    set up base method to get i/e prices sorted for hs4

---
+++ process_price_data.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import pandas as pd
+
+from jp_imports.data_process import DataProcess
+
+from utils.converter_utils import ConverterUtils
+
+
+def proccess_price_data() -> tuple[pd.DataFrame, pd.DataFrame]:
+    # Create ConverterUtils
+    cur_dir = Path(__file__).parent.resolve()
+    reference_file_path = str(cur_dir / "data" / "schedule_b_reference.xlsx")
+    utils = ConverterUtils(reference_file_path)
+
+    # Get i/e price data
+    df: pd.DataFrame = DataProcess("data/").process_price().collect().to_pandas()
+
+    df1 = df[["hs4", "price_imports"]]
+    df1 = (
+        df1.groupby(by="hs4")
+        .agg(sum)
+        .sort_values(by="price_imports", ascending=False)
+        .reset_index()
+    )
+
+    df2 = df[["hs4", "price_exports"]]
+    df2 = (
+        df2.groupby(by="hs4")
+        .agg(sum)
+        .sort_values(by="price_exports", ascending=False)
+        .reset_index()
+    )
+
+    return df1, df2
+
+
+if __name__ == "__main__":
+    df1, df2 = proccess_price_data()
+    print(df1, df2)
