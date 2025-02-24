a160b07de7e0e91df009058afad379dfd12c1e64
Alejandro Ouslan <unclearcoder@hotmail.com>
2025-02-24 21:52:40 +0000

    Hotfix 1 (#11)
    * update submodule
    
    * change the download link for the nutrition matrix
    
    * update submodule

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.data_process import DataCal
 
 
 def main() -> None:
-    print(DataCal().process_int_org(agg="monthly", types="hts").execute())
+    print(DataCal().process_int_org(time_frame="monthly", level="hts").execute())
 
 
 if __name__ == "__main__":
